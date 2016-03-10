8a77dcc3c91b6193e62711bb5b19fdccd86cb4a2
rbgwozdz <richard.gwozds@gmail.com>
2016-03-09 19:23:23 -0500

    Commit;

---
+++ WeatherRESTClient/src/com/dao/BaseDao.java
@@ -0,0 +1,25 @@
+package com.dao;
+
+import java.util.List;
+
+public interface BaseDao {
+	public List listAll(Class clazz);
+
+	public List listAllByName(Class clazz);
+
+	public Object find(Class clazz, Long id);
+
+	public Object find(Class clazz, String id);
+
+	public void save(Object obj);
+
+	public Object create(Object obj);
+
+	public void remove(Object obj);
+
+	public void remove(Class clazz, Long id);
+
+	public void remove(Class clazz, String id);
+
+	public Long count(Class clazz);
+}
